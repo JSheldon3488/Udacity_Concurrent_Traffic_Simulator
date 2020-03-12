6e85a1881f1d3effa6f6c762c2b40c9264687fb0
Justin Sheldon <JSheldon3488@Gmail.com>
2020-03-12 07:06:41 +0000

    Finished Project

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <future>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
@@ -12,8 +14,8 @@ T MessageQueue<T>::receive()
     // The received object should then be returned by the receive function. 
     std::unique_lock<std::mutex> ulock(_mutex);
     _cond.wait(ulock, [this](){ return !_queue.empty(); });
-    T msg = std::move(_queue.back());
-    _queue.pop_back();
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
     return msg;
 }
 
@@ -24,7 +26,7 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> lock(_mutex);
-    _queue.emplace_back(std::move(msg));
+    _queue.emplace_back(msg);
     _cond.notify_one();
 }
 
@@ -47,8 +49,7 @@ void TrafficLight::waitForGreen()
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
     while (true) {
-        TrafficLightPhase light_status = _messages.receive();
-        if (light_status == TrafficLightPhase::green) {
+        if (_messages.receive() == TrafficLightPhase::green) {
             return;
         }
     }
@@ -67,23 +68,24 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> uni(4000,6000); //Increased time to make graphic better
+    auto cycleDuration = uni(rng);
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
     while (true) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        //Get a random value between 4 and 6 (seconds for toggling light)
-        std::random_device rd;
-        std::mt19937 rng(rd());
-        std::uniform_int_distribution<int> uni(4,6);
-        auto random_int = uni(rng);
         //Calculate difference between start time and current time and toggle light if it has been long enough
-        long timeSinceLastChange = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count();
-        if (timeSinceLastChange > random_int) {
-            _currentPhase = this->getCurrentPhase() == TrafficLightPhase::red ? TrafficLightPhase::green : TrafficLightPhase::red; 
+        int timeSinceLastChange = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count();
+        if (timeSinceLastChange >= cycleDuration) {
+            //Toggle Light
+            _currentPhase = _currentPhase == TrafficLightPhase::red ? TrafficLightPhase::green : TrafficLightPhase::red; 
             //Send message
             _messages.send(std::move(_currentPhase));
             //Reset the start timer for a new cycle
             start = std::chrono::system_clock::now();
+            cycleDuration = uni(rng);
         }
     }
 }
